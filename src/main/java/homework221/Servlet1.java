02e49670b0488fa26aafdc4034a7bba003347950
korablev <o.korablev@bk.ru>
2022-06-20 19:37:05 +0300

    first commit

---
+++ src/main/java/homework221/Servlet1.java
@@ -0,0 +1,24 @@
+package homework221;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+public class Servlet1 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        try {
+            writer.println("<h3>Time: " + LocalDateTime.now() + "</h3>");
+        } finally {
+            writer.close();
+        }
+    }
+}
---
+++ src/main/java/homework221/ServletDate.java
@@ -0,0 +1,24 @@
+package homework221;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+
+public class ServletDate extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        try {
+            writer.println("<h2>Date: " + LocalDate.now() + "</h3>");
+        } finally {
+            writer.close();
+        }
+    }
+}
